fa895bae2382e5f14db73f04ed919f0f7dbe9993
Kotla.Lokeshwari <kotlalokeshwari1871@gmail.com>
2025-10-16 20:21:49 +0530

    Add frontend auth-using react-hook-Form zod

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import {useForm, type SubmitHandler} from 'react-hook-form'
+import {z} from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const userFieldsSchema=z.object({
+  email:z.string().email(),
+  password:z.string().min(8)
+})
+
+type UserFields=z.infer<typeof userFieldsSchema>
 
 const Login = () => {
+
+  const {register,handleSubmit,formState:{errors}}=useForm<UserFields>({
+    resolver:zodResolver(userFieldsSchema)
+  })
+
+
+  const onSubmit:SubmitHandler<UserFields>=(data)=>{
+     console.log(data);
+  }
+
+
   return (
-    <div>Login</div>
+    <form action="" onSubmit={handleSubmit(onSubmit)} className='flex flex-col'>
+        <label htmlFor="">Email:</label>
+        <input 
+           type="email" 
+           {...register("email")}
+        />
+        {errors.email && <div className="mt-1 text-sm text-red-600">{errors.email.message}</div>}
+        <label htmlFor="">Password:</label>
+        <input 
+          type="password" 
+          {...register("password")}
+        />
+        {errors.password && <div className="mt-1 text-sm text-red-600">{errors.password.message}</div>}
+        <button type='submit'>Submit</button>
+    </form>
   )
 }
 
