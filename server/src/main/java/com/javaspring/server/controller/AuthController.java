aacc6719f4cd91b27c71dd3a6b9d782fa3e48a7e
Kotla.Lokeshwari <kotlalokeshwari1871@gmail.com>
2025-10-20 19:33:30 +0530

    JWT token generation

--- server/src/main/java/com/javaspring/server/controller/AuthController.java
+++ server/src/main/java/com/javaspring/server/controller/AuthController.java
@@ -1,4 +1,38 @@
 package com.javaspring.server.controller;
 
+
+import com.javaspring.server.model.AuthRequest;
+import com.javaspring.server.util.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JWTUtil jwtUtil;
+
+    @PostMapping("/authenticate")
+    public String generateToken(@RequestBody
+                                AuthRequest authRequest){
+        try {
+            System.out.println(authRequest);
+            //here user is authenticated-username and password are checked
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+
+            //generate jwt token
+           return jwtUtil.generateToken(authRequest.getUsername());
+        }catch (Exception e){
+            throw e;
+        }
+    }
 }
--- server/src/main/java/com/javaspring/server/security/SecurityConfig.java
+++ server/src/main/java/com/javaspring/server/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
